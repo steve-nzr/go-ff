83997b1c66fabf18ac3c37962543a7b3875da780
Steve-Nzr <steve@theval.tech>
2018-11-10 21:38:55 +0100

    :wrench: New dotenv loading & fixed messaging expiration

--- common/service/dotenv/dotenv.go
+++ common/service/dotenv/dotenv.go
@@ -1,11 +1,21 @@
 package dotenv
 
 import (
+	"log"
+	"os"
+
 	"github.com/joho/godotenv"
 )
 
 // Initialize the service
 // Loads the .env file
 func Initialize() {
-	godotenv.Load("../config/.env")
+	filename, ok := os.LookupEnv("ENV_FILE")
+	if ok == false {
+		filename = ".env"
+	}
+	err := godotenv.Load("./config/" + filename)
+	if err != nil {
+		log.Print(err.Error())
+	}
 }
